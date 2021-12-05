bb2cc45080ce4c1827b4f37cebad01e11025a687
Sumit-dey <deysumit417@gmail.com>
2021-12-05 14:24:51 -0500

    phase-1 done

---
+++ backend/routes/api/index.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router
+
+
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+module.exports = router;
