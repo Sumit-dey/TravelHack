8c85233a0697826e1fffb43634b7229e2724e064
Sumit-dey <deysumit417@gmail.com>
2022-02-24 00:10:30 -0500

    AA

---
+++ backend/routes/api/deletereview.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+// const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User, Spot, Image, Review } = require('../../db/models');
+// const listingValidations = require('../../utils/listings')
+
+router.delete("/:id", asyncHandler(async function (req, res) {
+    const id = req.params.id
+    const review = await Review.findByPk(id)
+    await review.destroy()
+    return res.json(review);
+}));
+
+
+
+
+module.exports = router;
