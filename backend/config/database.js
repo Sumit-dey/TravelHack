1b407ce696bbb8bedc1927154e1265124a512ee5
Sumit-dey <deysumit417@gmail.com>
2023-07-23 05:13:45 -0400

    aa

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,12 +8,21 @@ const host = db.host;
 
 module.exports = {
   development: {
-    username,
-    password,
-    database,
-    host,
+    // username,
+    // password,
+    // database,
+    // host,
+    // dialect: 'postgres',
+    // seederStorage: 'sequelize'
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    seederStorage: 'sequelize'
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     use_env_variable: 'DATABASE_URL',
